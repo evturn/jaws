0cac86a543a23d8d11d42d3382e32b7827307f6f
Evan Turner <ev@evturn.com>
2017-12-15 13:09:14 -0500

    Insert submap as value entry in Map

--- src/Jaws.hs
+++ src/Jaws.hs
@@ -5,29 +5,15 @@ import           Data.List        (lines, words)
 import qualified Data.Map         as M
 import           Text.Show.Pretty (ppShow)
 
-type Mapping = M.Map String (String, Int)
-
-cleanChar :: Char -> Maybe Char
-cleanChar x
-    | isAlphaNum x = Just $ toLower x
-    | otherwise    = Nothing
-
-clean :: String -> String
-clean xs = foldr go [] $ (fmap cleanChar xs)
-  where
-    go (Just a) b = a : b
-    go Nothing b  = b
-
-insert' :: String -> String -> Int -> Mapping -> Mapping
-insert' x y n mp = M.insert (clean x) (clean y, n) mp
-
-inc :: (String, Int) -> (String, Int)
-inc (x, n) = (x, n + 1)
+type SubMapping = M.Map String Int
+type Mapping    = M.Map String SubMapping
 
 insert :: String -> String -> Mapping -> Mapping
-insert x y mp = case M.lookup (clean x) mp of
-                  Nothing      -> insert' x y 1 mp
-                  Just (x', n) -> insert' x y (n + 1) mp
+insert x y mp = case M.lookup x mp of
+  Nothing  -> M.insert x (M.singleton y 1) mp
+  Just smp -> case M.lookup y smp of
+                Nothing -> M.insert x (M.singleton y 1) mp
+                Just n  -> M.insert x (M.insert y (n + 1) smp) mp
 
 mapWords :: [[String]] -> Mapping
 mapWords xs = foldr go M.empty xs
