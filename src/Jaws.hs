73e6b649af2e4676c58d5277b2d977114fd5dfa0
Evan Turner <ev@evturn.com>
2017-12-14 23:16:00 -0500

    Remove non-alphanumeric chars from strings

--- src/Jaws.hs
+++ src/Jaws.hs
@@ -2,12 +2,23 @@ module Jaws where
 
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Maybe
+import           Data.Char                 (isAlphaNum, toLower)
 import           Data.List                 (lines, words)
-import           Data.Map
 import           System.Environment        (getArgs)
 import           Text.Show.Pretty          (ppShow)
 
 type WordMapping = (String, String, Int)
+
+cleanChar :: Char -> Maybe Char
+cleanChar x
+    | isAlphaNum x = Just $ toLower x
+    | otherwise    = Nothing
+
+cleanChars :: String -> String
+cleanChars xs = foldr go [] $ (fmap cleanChar xs)
+  where
+    go (Just a) b = a : b
+    go Nothing b  = b
 
 mapWords :: [String] -> [WordMapping]
 mapWords = go
