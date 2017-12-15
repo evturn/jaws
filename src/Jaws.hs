3237997cd03bbb0b86e3acc31a7674876cee2740
Evan Turner <ev@evturn.com>
2017-12-14 22:35:01 -0500

    Capture word and succeeding word

--- src/Jaws.hs
+++ src/Jaws.hs
@@ -2,14 +2,22 @@ module Jaws where
 
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Maybe
-import           Data.List                 (lines)
+import           Data.List                 (lines, words)
+import           Data.Map
 import           System.Environment        (getArgs)
 import           Text.Show.Pretty          (ppShow)
 
+type WordMapping = (String, String, Int)
+
+mapWords :: [String] -> [WordMapping]
+mapWords = go
+  where
+    go (x:xs:xss) = (x, xs, 1) : mapWords xss
+    go _          = []
 
 printContents :: FilePath -> IO ()
 printContents p = do
   x <- readFile p
-  y <- return $ lines x
+  y <- return $ concat $ fmap (mapWords . words) $ lines x
   putStrLn $ ppShow y
 
