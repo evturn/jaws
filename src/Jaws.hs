8e868eff542c41bdf015928ac6639a2c96b8564d
Evan Turner <ev@evturn.com>
2017-12-14 20:51:31 -0500

    Add pretty printing

--- src/Jaws.hs
+++ src/Jaws.hs
@@ -4,11 +4,12 @@ import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Maybe
 import           Data.List                 (lines)
 import           System.Environment        (getArgs)
+import           Text.Show.Pretty          (ppShow)
 
 
 printContents :: FilePath -> IO ()
 printContents p = do
   x <- readFile p
   y <- return $ lines x
-  print y
+  putStrLn $ ppShow y
 
