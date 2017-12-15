dccfa88f59bcc59cbc1dba28e5727cd6a9df4059
Evan Turner <ev@evturn.com>
2017-12-15 00:35:33 -0500

    Create nested mapping of neighboring strings

--- src/Jaws.hs
+++ src/Jaws.hs
@@ -1,43 +1,38 @@
 module Jaws where
 
-import           Control.Monad.IO.Class
-import           Control.Monad.Trans.Maybe
-import           Data.Char                 (isAlphaNum, toLower)
-import           Data.List                 (lines, words)
-import           System.Environment        (getArgs)
-import           Text.Show.Pretty          (ppShow)
+import           Data.Char        (isAlphaNum, toLower)
+import           Data.List        (lines, words)
+import           Text.Show.Pretty (ppShow)
 
 type Actual  = String
 type Cleaned = String
 type Next    = String
 
-data WordMapping = Mapping Cleaned (Actual, Next)
-  deriving (Eq, Show)
-
+data WordMapping a = No
+                   | Mapping a (WordMapping a)
+                   deriving (Eq, Show)
 
 cleanChar :: Char -> Maybe Char
 cleanChar x
     | isAlphaNum x = Just $ toLower x
     | otherwise    = Nothing
 
-cleanChars :: String -> Cleaned
+cleanChars :: String -> String
 cleanChars xs = foldr go [] $ (fmap cleanChar xs)
   where
     go (Just a) b = a : b
     go Nothing b  = b
 
-runClean :: String -> String -> WordMapping
-runClean xs ys = Mapping (cleanChars xs) (xs, cleanChars ys)
 
-mapWords :: [String] -> [WordMapping]
+mapWords :: [String] -> WordMapping String
 mapWords = go
   where
-    go (x:xs:xss) = runClean x xs : mapWords (xs:xss)
-    go _          = []
+    go (x:xs:xss) = Mapping (cleanChars x) (go (xs:xss))
+    go _          = No
 
 printContents :: FilePath -> IO ()
 printContents p = do
   x <- readFile p
-  y <- return $ concat $ fmap (mapWords . words) $ lines x
+  y <- return $ fmap (mapWords . words) $ lines x
   putStrLn $ ppShow y
 
