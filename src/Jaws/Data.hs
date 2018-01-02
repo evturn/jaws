6b34bf50b77ccdfa81491da5754681d992c9c36a
Evan Turner <ev@evturn.com>
2018-01-02 07:14:40 -0500

    Expose interface to run as lib code or executable

--- src/Jaws/Data.hs
+++ src/Jaws/Data.hs
@@ -1,9 +1,10 @@
 module Jaws.Data
     ( Map
     , Mapping
+    , getSeeds
     , Submap
     , insert
-    , insertSubmap
+    , insertSub
     , mapping
     ) where
 
@@ -19,14 +20,14 @@ newtype Mapping a = Mapping (Mapping' a)
 instance Show a => Show (Mapping a) where
   show = prettyShow
 
-insertSubmap :: Maybe Int -> String -> Submap -> Submap
-insertSubmap Nothing  k sp = M.insert k 1 sp
-insertSubmap (Just n) k sp = M.insert k (n + 1) sp
+insertSub :: Maybe Int -> String -> Submap -> Submap
+insertSub Nothing  k sp = M.insert k 1 sp
+insertSub (Just n) k sp = M.insert k (n + 1) sp
 
 insert :: String -> String -> Map -> Map
 insert k1 k2 mp = case M.lookup k1 mp of
   Nothing -> M.insert k1 (M.singleton k2 1) mp
-  Just sp -> M.insert k1 (insertSubmap (M.lookup k2 sp) k2 sp) mp
+  Just sp -> M.insert k1 (insertSub (M.lookup k2 sp) k2 sp) mp
 
 mapping :: String -> Map
 mapping = (foldr go M.empty) . wordsByLine
@@ -34,3 +35,14 @@ mapping = (foldr go M.empty) . wordsByLine
     go (x:xs:xss) mp = go (xs:xss) (insert x xs mp)
     go (x:[])     mp = insert x "" mp
     go []         mp = mp
+
+consInits :: String -> Submap -> [String] -> [String]
+consInits  k sp ks = case hasSuccessor sp of
+  True  -> k : ks
+  False -> ks
+
+getSeeds :: Map -> [String]
+getSeeds = M.foldrWithKey consInits []
+
+hasSuccessor :: Submap -> Bool
+hasSuccessor = M.notMember ""
--- src/Main.hs
+++ src/Main.hs
@@ -7,15 +7,12 @@ module Main
     , main
     ) where
 
-import           Control.Lens               hiding (mapping)
-import qualified Data.ByteString.Lazy.Char8 as Char8
-import qualified Data.Map                   as M
-import           Data.Maybe                 (fromMaybe)
+import           Control.Monad.Reader
+import qualified Data.Map             as M
+import           Data.Maybe           (fromMaybe)
 import           Jaws.Data
 import           Jaws.Internal
 import           Jaws.Text
-import           Network.Wreq
-import           System.Environment
 
 getSelections :: Maybe Submap -> [String]
 getSelections smp = foldr go [] $ M.toList (fromMaybe M.empty smp)
@@ -23,40 +20,22 @@ getSelections smp = foldr go [] $ M.toList (fromMaybe M.empty smp)
     go (k, v) xs = xs ++ replicate v k
 
 sumElems :: Maybe Submap -> Int
-sumElems Nothing     = 0
-sumElems (Just smps) = sum $ M.elems smps
+sumElems Nothing    = 0
+sumElems (Just sps) = sum $ M.elems sps
 
-getInitValues :: Map -> [String]
-getInitValues mp = M.foldrWithKey go [] mp
-  where
-    go k smp ks = case M.notMember "" smp of
-                    True  -> k : ks
-                    False -> ks
+seeds :: ReaderT Map [] String
+seeds = ReaderT $ \r -> getSeeds r
 
-getInitValue :: Map -> IO String
-getInitValue mp = do
-  seeds <- return $ getInitValues mp
-  index <- getRandomInt (length $ seeds)
-  return $ seeds !! index
+getSeedValue :: [String] -> IO String
+getSeedValue sds = do
+  index <- getRandomInt (length $ sds)
+  return $ sds !! index
 
 getInitState :: Map -> IO (String, String)
 getInitState mp = do
-  seed <- getInitValue mp
+  sds <- return $ getSeeds mp
+  seed <- getSeedValue sds
   return (seed, (caps seed))
-
-fromFile :: FilePath -> IO String
-fromFile p = do
-  readFile p
-
-fromURL :: String -> IO String
-fromURL url = do
-  r  <- get url
-  xs <- return $ r ^. responseBody
-  return $ Char8.unpack xs
-
-getSourceString :: String -> String -> IO String
-getSourceString  "file" y = fromFile y
-getSourceString  _ y      = fromURL y
 
 runBuilder :: (String, String) -> Map -> IO String
 runBuilder state mp = do
@@ -69,19 +48,19 @@ runBuilder state mp = do
     "" -> return $ snd state
     _  -> runBuilder (word, (snd state ++ " " ++ word)) mp
 
-runJaws :: Map -> IO String
-runJaws mp = do
-  state <- getInitState mp
-  runBuilder state mp
+run :: String -> IO ()
+run xs = do
+  mp <- return $ mapping xs
+  st <- getInitState mp
+  x  <- runBuilder st mp
+  prettyPrint x
 
 jaws :: String -> String -> IO ()
-jaws x s = do
-  xs     <- getSourceString x s
-  mp     <- return $ mapping xs
-  result <- runJaws mp
-  prettyPrint result
+jaws mtd loc = do
+  xs <- runJaws mtd loc
+  run xs
 
 main :: IO ()
 main = do
-  (t:p:[]) <- getArgs
-  jaws t p
+  xs <- execJaws
+  run xs
