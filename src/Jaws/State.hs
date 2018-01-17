5c831d993539e5df81ce2c06a71d90c522d211d9
Evan Turner <ev@evturn.com>
2018-01-16 23:08:51 -0500

    Add state and IO handlers

--- src/Jaws/State.hs
+++ src/Jaws/State.hs
@@ -1,26 +1,46 @@
 module Jaws.State where
 
-import           Data.IORef
-import           Jaws.Data     (Map, keys, mapping)
-import           Jaws.Internal (pick)
+import           Jaws.Data     (Map, Submap, getSub, keys, mapping,
+                                probabilities, subToList, subValues)
+import           Jaws.Internal (getRandomInt, pick)
 import           Jaws.Text     (caps)
 
-makeMap :: FilePath -> IO (IORef Map)
-makeMap fp = do
-  xs <- readFile fp
-  newIORef $ mapping xs
+getState :: String -> (String, String)
+getState = (,) <$> id <*> caps
 
-makeState :: String -> (String, String)
-makeState = (,) <$> id <*> caps
+emptyS :: (String, String)
+emptyS = (mempty, mempty)
 
-getBuilderState :: IORef Map -> IO (String, String)
-getBuilderState mapRef = do
-  mp <- readIORef mapRef
-  seed  <- pick $ keys mp
-  return $ makeState seed
+sumSubValues :: Submap -> Int
+sumSubValues sub = sum $ subValues sub
+
+mergeState :: (String, String) -> (String, String) -> (String, String)
+mergeState sta sta' = (fst sta', (snd sta) ++ " " ++ (snd sta'))
+
+putState :: [String] -> (String, String) -> IO (String, String)
+putState seeds sta = do
+  seed <- pick seeds
+  sta' <- return $ getState seed
+  case sta of
+    ("", "") -> return sta'
+    _        -> return $ mergeState sta sta'
+
+buildState :: [String] -> (String, String) -> Map -> IO String
+buildState seeds sta mp = do
+  sub        <- return $ getSub (fst sta) mp
+  total      <- return $ sumSubValues sub
+  selections <- return $ probabilities sub
+  index      <- getRandomInt total
+  word       <- return $ selections !! index
+  case word of
+    "" -> return $ snd sta
+    _  -> buildState seeds (word, (snd sta ++ " " ++ word)) mp
 
 start :: String -> IO ()
 start path = do
-  mapRef <- makeMap path
-  st <- getBuilderState mapRef
+  xs    <- readFile path
+  mp    <- return $ mapping xs
+  seeds <- return $ keys mp
+  sta   <- putState seeds emptyS
+  st    <- buildState seeds sta mp
   print st
