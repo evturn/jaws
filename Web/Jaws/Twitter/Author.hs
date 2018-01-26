428d8b09c4f83558534cdee51ce1e4cadc0b7632
Evan Turner <ev@evturn.com>
2018-01-26 12:52:46 -0500

    Read JSON bytestring and get env vals

--- Web/Jaws/Twitter/Author.hs
+++ Web/Jaws/Twitter/Author.hs
@@ -7,27 +7,63 @@ module Web.Jaws.Twitter.Author where
 
 import           Data.Aeson
 import           Data.Aeson.Types
-import qualified Data.ByteString.Lazy as BS
-import qualified Data.Text            as T
-import           GHC.Generics
+import qualified Data.ByteString.Char8 as S8
+import qualified Data.ByteString.Lazy  as B
+import           System.Environment
+import           Web.Twitter.Conduit
 
 data Author = Author
     { consumerKey    :: String
     , consumerSecret :: String
     , accessToken    :: String
     , accessSecret   :: String
-    } deriving (Show, Generic, ToJSON, FromJSON)
+    } deriving Show
 
-parseObj :: Value -> Parser Author
-parseObj = withObject "object" $ \o ->
-  Author <$> o .: "consumerKey"
-         <*> o .: "consumerSecret"
-         <*> o .: "accessToken"
-         <*> o .: "accessSecret"
+instance FromJSON Author where
+  parseJSON (Object x) = Author
+    <$> x .: "consumerKey"
+    <*> x .: "consumerSecret"
+    <*> x .: "accessToken"
+    <*> x .: "accessSecret"
 
-parseItUp :: IO (Parser Author)
+instance ToJSON Author where
+  toJSON (Author ck cs at as) = object
+    [ "consumerKey"    .= ck
+    , "consumerSecret" .= cs
+    , "accessToken"    .= at
+    , "accessSecret"   .= as
+    ]
+
+getJSON :: IO B.ByteString
+getJSON = B.readFile "/Users/ev/src/dev/jaws/env.json"
+
+maybeAuthor :: IO (Maybe Author)
+maybeAuthor = decode <$> getJSON
+
+parseItUp :: IO ()
 parseItUp = do
-  json <- BS.readFile "/Users/ev/src/dev/jaws/env.json"
-  case decode json of
-    Nothing -> return $ fail "shit fuck!"
-    Just x  -> return $ parseObj (toJSON $ Object x)
+  d <- maybeAuthor
+  case d of
+    Nothing -> return ()
+    Just x  -> do
+      val <- authTokens x
+      print val
+
+authTokens :: Author -> IO (OAuth, Credential)
+authTokens x = do
+  ck <- env . consumerKey $ x
+  cs <- env . consumerSecret $ x
+  at <- env . accessToken $ x
+  as <- env . accessSecret $ x
+  let oauth = twitterOAuth
+            { oauthConsumerKey =  ck
+            , oauthConsumerSecret = cs
+            }
+      cred  = Credential
+            [ ("oauth_token", at)
+            , ("oauth_token_secret", as)
+            ]
+  return (oauth, cred)
+
+env :: String -> IO S8.ByteString
+env = (S8.pack <$>) . getEnv
