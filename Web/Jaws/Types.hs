07bc7f2c89b4df8541d26d4a352c4470f4d9dfac
Evan Turner <ev@evturn.com>
2018-01-28 22:23:22 -0500

    Create directory for types

--- Web/Jaws/Types.hs
+++ Web/Jaws/Types.hs
@@ -4,15 +4,4 @@ module Web.Jaws.Types
     , Submap
     ) where
 
-import qualified Data.Map      as M
-import           Web.Jaws.Text (prettyShow)
-
-type MP a     = M.Map String a
-type Map      = MP Submap
-type Submap   = MP Int
-
-newtype Mapping a = Mapping (MP a)
-
-instance Show a => Show (Mapping a) where
-  show = prettyShow
-
+import           Web.Jaws.Types.Mapping
---
+++ Web/Jaws/Types/Mapping.hs
@@ -0,0 +1,18 @@
+module Web.Jaws.Types.Mapping
+    ( Map
+    , Mapping
+    , Submap
+    ) where
+
+import qualified Data.Map      as M
+import           Web.Jaws.Text (prettyShow)
+
+type MP a     = M.Map String a
+type Map      = MP Submap
+type Submap   = MP Int
+
+newtype Mapping a = Mapping (MP a)
+
+instance Show a => Show (Mapping a) where
+  show = prettyShow
+
