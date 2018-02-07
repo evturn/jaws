a39977895cbb03f506fc89fcfcc3b09d7f2b37ed
Evan Turner <ev@evturn.com>
2018-02-07 10:25:17 -0500

    Rewrite recursion for building content

--- src/Jaws/Data/State.hs
+++ src/Jaws/Data/State.hs
@@ -1,39 +1,36 @@
 module Jaws.Data.State where
 
 import           Data.Char          (toUpper)
-import           Data.Monoid
 import           Jaws.Data.Mapping  (Map, keys, lookupSubmap, mapping,
                                      wordFrequencyList)
 import           Jaws.System.Random (randomSelect)
 
+initState :: [String] -> IO (String, String)
+initState seeds = do
+  injectState <$> randomSelect seeds
+
 injectState :: String -> (String, String)
 injectState = (,) <$> id <*> caps
 
-emptyState :: (String, String)
-emptyState = (mempty, mempty)
+caps :: String -> String
+caps xs = (toUpper . head) xs : tail xs
 
-initState :: [String] -> (String, String) -> IO (String, String)
-initState seeds sta = do
-  sta' <- fmap injectState $ randomSelect seeds
-  return $ case sta of
-    ("", "") -> sta'
-    _        -> (fst sta', (snd sta) ++ " " ++ (snd sta'))
+nextWord :: Map -> String -> IO String
+nextWord mp x = randomSelect $ wordFrequencyList (lookupSubmap x mp)
 
-buildState :: [String] -> (String, String) -> Map -> IO String
-buildState seeds sta mp = do
-  sub           <- return $ lookupSubmap (fst sta) mp
-  wordSelection <- randomSelect $ wordFrequencyList sub
-  case wordSelection of
-    "" -> return $ snd sta
-    _  -> buildState seeds (wordSelection, (snd sta ++ " " ++ wordSelection)) mp
+build :: (String -> IO String) -> (String, String) -> IO String
+build f (x, s) = do
+  y <- f x
+  case y of
+    "" -> return s
+    _  -> build f (y, s ++ " " ++ y)
+
+execState :: [String] -> Map -> IO String
+execState seeds mp = do
+  initState seeds >>= (build (nextWord mp))
 
 runJaws :: String -> IO String
 runJaws xs = do
   mp    <- return $ mapping xs
   seeds <- return $ keys mp
-  sta   <- initState seeds emptyState
-  buildState seeds sta mp
-
-caps :: String -> String
-caps xs = (toUpper . head) xs : tail xs
-
+  execState seeds mp
