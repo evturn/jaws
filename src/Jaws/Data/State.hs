e42c2c8aeb9613b09ae16023ca5842efe8bca7a1
Evan Turner <ev@evturn.com>
2018-02-07 09:24:47 -0500

    Clean up state initialization

--- src/Jaws/Data/State.hs
+++ src/Jaws/Data/State.hs
@@ -6,26 +6,22 @@ import           Jaws.Data.Mapping  (Map, keys, lookupSubmap, mapping,
                                      wordFrequencyList)
 import           Jaws.System.Random (randomSelect)
 
+injectState :: String -> (String, String)
+injectState = (,) <$> id <*> caps
+
 emptyState :: (String, String)
 emptyState = (mempty, mempty)
 
-getState :: String -> (String, String)
-getState = (,) <$> id <*> caps
-
-mergeState :: (String, String) -> (String, String) -> (String, String)
-mergeState sta sta' = (fst sta', (snd sta) ++ " " ++ (snd sta'))
-
-putState :: [String] -> (String, String) -> IO (String, String)
-putState seeds sta = do
-  seed <- randomSelect seeds
-  sta' <- return $ getState seed
-  case sta of
-    ("", "") -> return sta'
-    _        -> return $ mergeState sta sta'
+initState :: [String] -> (String, String) -> IO (String, String)
+initState seeds sta = do
+  sta' <- fmap injectState $ randomSelect seeds
+  return $ case sta of
+    ("", "") -> sta'
+    _        -> (fst sta', (snd sta) ++ " " ++ (snd sta'))
 
 buildState :: [String] -> (String, String) -> Map -> IO String
 buildState seeds sta mp = do
-  sub  <- return $ lookupSubmap (fst sta) mp
+  sub           <- return $ lookupSubmap (fst sta) mp
   wordSelection <- randomSelect $ wordFrequencyList sub
   case wordSelection of
     "" -> return $ snd sta
@@ -35,7 +31,7 @@ runJaws :: String -> IO String
 runJaws xs = do
   mp    <- return $ mapping xs
   seeds <- return $ keys mp
-  sta   <- putState seeds emptyState
+  sta   <- initState seeds emptyState
   buildState seeds sta mp
 
 caps :: String -> String
