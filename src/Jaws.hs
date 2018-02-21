670522440af8961b24f52a4c7c16c57e6e1bde03
Evan Turner <ev@evturn.com>
2018-02-20 22:23:22 -0500

    Move top level functions

--- src/Jaws.hs
+++ src/Jaws.hs
@@ -4,26 +4,9 @@ module Jaws
     ( module Jaws.Data
     , module Jaws.System
     , module Jaws.Twitter
-    , jaws
-    , jawsR
-    , once
     ) where
 
 import           Jaws.Data
 import           Jaws.System
 import           Jaws.Twitter
 
-once :: String -> IO String
-once loc = do
-  xs <- fetchSource loc
-  runJaws $ mapping xs
-
-jaws :: Int -> String -> IO String
-jaws n loc = do
-  xs <- fetchSource loc
-  runRepeat n $ mapping xs
-
-jawsR :: Int -> String -> IO String
-jawsR n loc = do
-  xs <- fetchSource loc
-  runRepeatR n $ mapping xs
